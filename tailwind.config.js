0cd50156df745c2fcb0780fca0e2ca6994080991
Georgia Stuart <georgia.stuart@gmail.com>
2022-02-13 11:57:11 -0600

    Update fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,13 @@ module.exports = {
         "acm-yellow": '#FFD600',
         "acm-orange": '#FC9200',
         'acm-light-blue': {  DEFAULT: '#83CEE2',  '50': '#FFFFFF',  '100': '#FFFFFF',  '200': '#E6F5F9',  '300': '#C5E8F1',  '400': '#A4DBEA',  '500': '#83CEE2',  '600': '#56BCD7',  '700': '#2EA6C6',  '800': '#248099',  '900': '#195A6B'},
-        "acm-green": '#A6BC09',
+        'acm-green': {  DEFAULT: '#A6BC09',  '50': '#EBF984',  '100': '#E7F870',  '200': '#E1F649',  '300': '#DBF422',  '400': '#C8E30B',  '500': '#A6BC09',  '600': '#778606',  '700': '#475104',  '800': '#181B01',  '900': '#000000'},
         "acm-purple": '#65016B',
         "acm-dark-blue": '#09357A'
+      },
+      fontFamily: {
+        'coda': ['"Coda"', 'cursive'],
+        'open-sans': ['"Open Sans"', 'sans-serif']
       }
     },
     container: {
